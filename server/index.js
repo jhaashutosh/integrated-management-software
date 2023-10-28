919b52997ca54f58d2237f9b9110c8988b0182ad
ashutosh <jhaashutosh58@gmail.com>
2023-10-28 14:39:02 +0530

    28-10-2023: build deployed

--- server/index.js
+++ server/index.js
@@ -39,10 +39,8 @@ connectDB()
 
     if (process.env.NODE_ENV === "production") {
       app.use(express.static(path.join(__dirname, "../client/build")));
-      app.get(
-        "*",
-        (req, res) =>
-          res.sendFile(path.join(__dirname, "../client/build/index.html"))
+      app.get("*", (req, res) =>
+        res.sendFile(path.join(__dirname, "../client/build/index.html"))
       );
     } else {
       app.get("/", (req, res) => res.send("API is running"));
