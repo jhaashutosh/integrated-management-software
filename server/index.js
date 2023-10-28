17329270210ac513ee78f1cb9083061acd50dd83
Ashutosh Jha <73036341+jhaashutosh@users.noreply.github.com>
2023-10-28 13:03:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const {
   notFoundHandler,
   errorHandler,
@@ -7,32 +8,53 @@ const {
 const connectDB = require("./utils/db");
 const path = require("path");
 
-const port = process.env.PORT;
 const app = express();
 
-connectDB();
+// Load environment variables from a .env file
+dotenv.config();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Define the port (you can use a default value if it's not specified in the environment)
+const port = process.env.PORT || 5001;
 
-app.use("/api/units", require("./routes/unitRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/bills", require("./routes/billRoutes"));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
-  );
-} else {
-  app.get("/", (req, res) => res.send("API is running"));
+// Define a function to start the server
+function startServer() {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
 }
 
-app.use(notFoundHandler);
-app.use(errorHandler);
+// Connect to the database
+connectDB()
+  .then(() => {
+    console.log("Connected to the database");
 
-const PORT = process.env.port || 5001;
+    // Add middleware for parsing JSON and URL-encoded data
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+    // Define your API routes
+    app.use("/api/units", require("./routes/unitRoutes"));
+    app.use("/api/users", require("./routes/userRoutes"));
+    app.use("/api/bills", require("./routes/billRoutes"));
+
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, "../client/build"));
+      app.get(
+        "*",
+        (req, res) =>
+          res.sendFile(path.join(__dirname, "../client/build/index.html"))
+      );
+    } else {
+      app.get("/", (req, res) => res.send("API is running"));
+    }
+
+    // Error handling middleware
+    app.use(notFoundHandler);
+    app.use(errorHandler);
+
+    // Start the server
+    startServer();
+  })
+  .catch((error) => {
+    console.error(`Database connection error: ${error.message}`);
+  });
